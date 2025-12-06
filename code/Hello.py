ae090aadb3602ced6ec8ebf48d7a91507e313853
htkhanh05 <hatuankhanh09@gmail.com>
2025-12-07 02:43:35 +0700

    add hello

---
+++ code/Hello.py
@@ -0,0 +1,14 @@
+'''
+    Author: htkhanh05
+    Created at: Sunday, 2025-12-07 02:42:37
+'''
+
+def solve():
+    name = input()
+    print(f"Hello {name}!")
+
+if __name__ == '__main__':
+    t = 1
+    #t = int(input())
+    for _ in range(t):
+        solve()
